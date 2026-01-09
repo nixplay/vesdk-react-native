imgly/pesdk-react-native

Add version 3.3.0

index.js
@@ -157,11 +157,7 @@ class PESDK {
    * resolved by the packager.
    */
   static unlockWithLicense(license) {
-    if (Platform.OS == 'android') {
-      return RNPhotoEditorSDK.unlockWithLicense(JSON.stringify(license));
-    } else {
-      return RNPhotoEditorSDK.unlockWithLicense(license);
-    }
+    return RNPhotoEditorSDK.unlockWithLicense(JSON.stringify(license));
   }
 }
 

nixplay/vesdk-react-native

index.js
@@ -203,11 +203,7 @@ class VESDK {
    * resolved by the packager.
    */
   static unlockWithLicense(license) {
-    if (Platform.OS == 'android') {
-      return RNVideoEditorSDK.unlockWithLicense(JSON.stringify(license));
-    } else {
-      return RNVideoEditorSDK.unlockWithLicense(license);
-    }
+    return RNVideoEditorSDK.unlockWithLicense(JSON.stringify(license));
   }
 
 //RRC try to override behavior here to prepare for language update